72ad140248e65baff0783db09ab7fc9eb3837f9b
yiwei1029 <rot1998@163.com>
2024-01-05 22:02:39 +0800

    login界面

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import './plugins/element.js'
 import './assets/css/global.css'
+import './assets/font/iconfont.css'
 Vue.config.productionTip = false
 
 new Vue({
