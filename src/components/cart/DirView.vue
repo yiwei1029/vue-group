1bd0c54f65fd92f31c7f9534329ea1cca9fb216f
yiwei1029 <rot1998@163.com>
2024-01-22 16:47:40 +0800

    界面优化

--- src/components/cart/DirView.vue
+++ src/components/cart/DirView.vue
@@ -46,18 +46,18 @@
             </el-table-column>
 
         </el-table>
-        <el-row :gutter="20" class="cart_total">
-            <el-col :span="6"> Total amount: {{ total_num }}</el-col>
-            <el-col :span="6"> Sum: {{ total_price }}</el-col>
+        <el-row :gutter="20" class="cart_total" type="flex" justify="end">
+            <el-col :span="6" style="text-align: right;"> Total amount: {{ total_num }}</el-col>
+            <el-col :span="6" style="text-align: right;"> Sum of price: ￥{{ total_price }}</el-col>
         </el-row>
-        <br/>
+        <br />
         <!-- 三種算法選擇 -->
         <el-row :gutter="20" class="algo_option" type="flex">
-            <el-col :span="6"><el-card><el-button type="primary">Warehouse</el-button></el-card> </el-col>
-            <el-col :span="6"><el-card><el-button type="success">Greedy</el-button></el-card></el-col>
-            <el-col :span="6"><el-card><el-button type="info">Random</el-button></el-card></el-col>
+            <el-col :span="8"><el-button type="primary" @click="createChart">Warehouse</el-button> </el-col>
+            <el-col :span="8"><el-button type="success" @click="createChart">Greedy</el-button></el-col>
+            <el-col :span="8"><el-button type="info" @click="createChart">Random</el-button></el-col>
         </el-row>
-        <br/>
+        <br />
         <el-row class="rebate-chart">
             <el-card>
                 <div>Rebates amount of every plan</div>
@@ -84,8 +84,9 @@ export default {
                 { name: 'T-shirt', price: 299, store: 'Adidas', amount: 1, goodsimg: "https://mobile.yoox.com/images/items/10/10087450OM_14_f.jpg?impolicy=crop&width=387&height=490" }],
             Rebate: [{ algo_name: 'WareHouse-First', rebate: 50 },
             { algo_name: 'Baseline-Greedy', rebate: 20 },
-            { algo_name: 'Baseline-Random', rebate: 10 }]
+            { algo_name: 'Baseline-Random', rebate: 10 }],
         }
+
     },
     methods: {
         async getCartList() {
@@ -101,39 +102,43 @@ export default {
                 console.log(item.amount)
             });
         },
+        createChart() {
+            var myChart = echarts.init(document.getElementById('chart1'));
+
+            // 指定图表的配置项和数据
+            var option = {
+                // title: {
+                //     text: 'Rebate'
+                // },
+                tooltip: {},
+                legend: {
+                    // data: ['Rebate']
+                },
+                xAxis: {
+                    data: this.Rebate.map(item => item.algo_name)
+                },
+                yAxis: {},
+                series: [
+                    {
+                        // name: 'Rebate',
+                        type: 'bar',
+                        data: this.Rebate.map(item => item.rebate)
+                    }
+                ],
+
+            };
+
+            // 使用刚指定的配置项和数据显示图表。
+            myChart.setOption(option);
+
+        }
 
     },
     created() {
         // this.getProdList()
     },
     mounted() {
-        var myChart = echarts.init(document.getElementById('chart1'));
 
-        // 指定图表的配置项和数据
-        var option = {
-            // title: {
-            //     text: 'Rebate'
-            // },
-            tooltip: {},
-            legend: {
-                // data: ['Rebate']
-            },
-            xAxis: {
-                data: this.Rebate.map(item => item.algo_name)
-            },
-            yAxis: {},
-            series: [
-                {
-                    // name: 'Rebate',
-                    type: 'bar',
-                    data: this.Rebate.map(item => item.rebate)
-                }
-            ],
-
-        };
-
-        // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
     },
     computed: {
         total_num: function () {
